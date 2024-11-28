72bece205f336251d371e8925af96dfde3ef6b47
godcandidate <godcandidate101@gmail.com>
2024-11-28 10:19:59 +0000

    update server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app";
 import 'dotenv/config';
 
 
-//create server
+
+//create server 
 app.listen(process.env.PORT, () => {
   console.log(`Server is connected http://localhost:${process.env.PORT}`);
   //connectDB();
