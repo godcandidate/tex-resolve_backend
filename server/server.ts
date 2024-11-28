e713b2676c917bab4374495dc8fe75e8c8d11284
godcandidate <godcandidate101@gmail.com>
2024-11-28 13:21:11 +0000

    feat: add db.ts for MongoDB connection and configure server.ts to connect to the database

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,10 @@
 import { app } from "./app";
 import 'dotenv/config';
-
+import connectDB from "./utils/db";
 
 
 //create server 
 app.listen(process.env.PORT, () => {
   console.log(`Server is connected http://localhost:${process.env.PORT}`);
-  //connectDB();
+  connectDB();
 });
