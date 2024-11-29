8eb4252ab2f8671576e4c1ed58029dfb9b7dd855
godcandidate <godcandidate101@gmail.com>
2024-11-29 08:25:25 +0000

    feat: add user controller with registration function

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,100 @@
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middlewares/catchAsyncError";
+import jwt, { Secret } from "jsonwebtoken";
+
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendEmail";
+
+
+import "dotenv/config";
+
+
+//using interface for req.user
+declare module "express" {
+  interface Request {
+    user?: IUser;
+  }
+}
+
+//Register user interface
+interface IRegistrationBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+//Register user
+export const registerUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body;
+
+      //Check if email already exists
+      const isEmailExist = await userModel.findOne({ email });
+      if (isEmailExist) {
+        return next(new ErrorHandler("Email already exists", 400));
+      }
+
+      const user: IRegistrationBody = {
+        name,
+        email,
+        password,
+      };
+
+      const activationToken = createActivationToken(user);
+      const activationCode = activationToken.activationCode;
+
+      const data = { user: { name: user.name }, activationCode };
+      await ejs.renderFile(
+        path.join(__dirname, "../mails/activation-mail.ejs"),
+        data
+      );
+
+      //send email to user
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Activate your account",
+          template: "activation-mail.ejs",
+          data,
+        });
+
+        res.status(201).json({
+          success: true,
+          message: `Please check your email: ${user.email} to activate your account!`,
+          activationToken: activationToken.token,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//Generate user activation token
+interface IActivationToken {
+  token: string;
+  activationCode: string;
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+  return { token, activationCode };
+};
