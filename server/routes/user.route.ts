f9981136432a927d9a03aebe82c503cf78392644
godcandidate <godcandidate@gmail.com>
2024-12-02 05:43:58 +0000

    feat: add login route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, registerUser} from "../controllers/user.controller";
+import { registerUser, activateUser, loginUser} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
@@ -7,5 +7,8 @@ const userRouter = express.Router();
 userRouter.post("/registration", registerUser);
 
 userRouter.post("/activate-user", activateUser);
+
+userRouter.post("/login", loginUser);
+
 
 export default userRouter;
