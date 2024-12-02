06d96c59a860b864db279fdbb763354b201fa355
godcandidate <godcandidate101@gmail.com>
2024-12-02 09:09:39 +0000

    fix: remove mocker for redis

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -1,19 +1,7 @@
-// tests/app.test.ts
 import request from 'supertest';
 import { app } from '../app'; 
 import { closeRedisConnection } from '../utils/redis';
 
-jest.mock('ioredis', () => {
-  return {
-      Redis: jest.fn(() => ({
-          status: 'ready',
-          quit: jest.fn().mockResolvedValue('OK'),
-          pipeline: jest.fn(() => ({
-              exec: jest.fn().mockResolvedValue([]),
-          })),
-      })),
-  };
-});
 
 describe('Test API Endpoints', () => {
   it('should respond with 200 and success message for /test route', async () => {
