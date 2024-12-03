a1cf56daf08c509c7b360047b97ef1e9c5e9ed0c
godcandidate <godcandidate@gmail.com>
2024-12-03 04:31:19 +0000

    feat: add user logout functionality and integrate logout route

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,11 +4,10 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middlewares/catchAsyncError";
 import jwt, { Secret } from "jsonwebtoken";
 
-
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendEmail";
-
+import { redis } from "../utils/redis";
 
 import "dotenv/config";
 import { sendToken } from "../utils/jwt";
@@ -175,6 +174,28 @@ export const loginUser = CatchAsyncError(
         return next(new ErrorHandler("Invalid email or password", 400));
       }
       sendToken(user, 200, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//logout user
+export const logoutUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+
+      // delete cache from redis
+      const userId = req.user?._id as string;
+
+      redis.del(userId);
+
+      res.status(200).json({
+        success: true,
+        message: "logged out successfully",
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { registerUser, activateUser, loginUser} from "../controllers/user.controller";
+import { registerUser, activateUser, loginUser, logoutUser} from "../controllers/user.controller";
+import { isAuthenticated } from "../utils/auth";
 
 const userRouter = express.Router();
 
@@ -9,6 +10,8 @@ userRouter.post("/registration", registerUser);
 userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
+
+userRouter.post("/logout", isAuthenticated, logoutUser);
 
 
 export default userRouter;
