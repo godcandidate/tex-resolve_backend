d7b61e41f3a5bd563c118982902b32e99bf5e111
godcandidate <godcandidate@gmail.com>
2024-12-03 04:50:46 +0000

    feat: added a console log to display redis connection

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -9,7 +9,7 @@ const redisClient = () => {
     }
 
     if (process.env.REDIS_URL) {
-        //console.log('Redis connected');
+        console.log('Redis connected');
         return process.env.REDIS_URL;
     }
     throw new Error('Redis connection failed');
