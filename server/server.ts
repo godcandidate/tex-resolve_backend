f60616916b94f7fdc9b7104c2b3a1eebd3c61e86
godcandidate <godcandidate101@gmail.com>
2025-02-11 07:15:46 +0000

    chore: monitor memory leaks

--- server/server.ts
+++ server/server.ts
@@ -10,3 +10,15 @@ app.listen(PORT, () => {
   console.log(`Server is connected http://localhost:${process.env.PORT}`);
   connectDB();
 });
+
+// Memory Usage Monitoring
+setInterval(() => {
+  const memoryUsage = process.memoryUsage();
+  console.log(`
+    🧠 Memory Usage:
+    🟢 RSS: ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB
+    🔵 Heap Total: ${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB
+    🔴 Heap Used: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB
+    🟡 External: ${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB
+  `);
+}, 5000); // Logs every 5 seconds
