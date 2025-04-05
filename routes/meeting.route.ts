705d0e2a0e16deba77a3e03ea3ea0933de39fbb9
godcandidate <godcandidate@gmail.com>
2025-04-05 11:29:35 +0000

    feat: update meeting route
    - add a route to get user meetings

--- routes/meeting.route.ts
+++ routes/meeting.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuthenticated } from "../utils/auth";
-import { createMeeting } from "../controllers/meeting.controller";
+import { createMeeting, getUserMeetings } from "../controllers/meeting.controller";
 
 const meetingRouter = express.Router();
 
 meetingRouter.post("/meetings", isAuthenticated, createMeeting);
+meetingRouter.get("/meetings/me", isAuthenticated, getUserMeetings);
 
 export default meetingRouter;
