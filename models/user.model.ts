675732a46f5336e29d98d87ceaa0ae6b39232104
godcandidate <godcandidate@gmail.com>
2025-04-05 11:56:32 +0000

    feat: update user models
    - added user account types attributes

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,63 +1,71 @@
-import mongoose, {Document, Model, Schema} from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import 'dotenv/config';
+import "dotenv/config";
 
 //email regex checker
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-export interface IUser extends Document{
-    name: string;
-    email:string;
-    password: string;
-    avatar: {
-        public_id: string;
-        url: string;
-    },
-    role:string;
-    isVerified: boolean;
-    comparePassword: (password: string) => Promise<boolean>;
-    SignAccessToken: () => string;
-    SignRefreshToken: () => string;
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  account_type: string;
+  comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
 }
 
 //User Schema
-const userSchema : Schema<IUser> = new mongoose.Schema({
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
     name: {
-        type : String,
-        required: [true, "Enter username"]
+      type: String,
+      required: [true, "Enter username"],
     },
     email: {
-        type : String,
-        required: [true, "Enter username"],
-        validate: {
-            validator: function (value: string) {
-              return emailRegexPattern.test(value);
-            },
-            message: "please enter a valid email",
-          }
+      type: String,
+      required: [true, "Enter username"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "please enter a valid email",
+      },
     },
     password: {
-        type : String,
-        required: [true, "Enter username"]
+      type: String,
+      required: [true, "Enter username"],
     },
     avatar: {
-        public_id: String,
-        url: String,
-      },
-      role: {
-        type: String,
-        default: "user",
-      },
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
+      public_id: String,
+      url: String,
     },
-    { timestamps: true }
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    account_type: {
+      type: String,
+      enum: ["freeUser", "proUser"],
+      default: "freeUser",
+    },
+  },
+
+  { timestamps: true }
 );
- 
+
 // sign access token
 userSchema.methods.SignAccessToken = function () {
   return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
@@ -74,19 +82,19 @@ userSchema.methods.SignRefreshToken = function () {
 
 // Hash Password before saving
 userSchema.pre<IUser>("save", async function (next) {
-    if (!this.isModified("password")) {
-      next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+  if (!this.isModified("password")) {
     next();
-  });
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 // compare password
 userSchema.methods.comparePassword = async function (
-    enteredPassword: string
-  ): Promise<boolean> {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-  const userModel: Model<IUser> = mongoose.model("User", userSchema);
-  export default userModel;
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel;
