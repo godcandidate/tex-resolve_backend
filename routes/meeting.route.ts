b2dac548ac8f22bf0f098469062552f5b70ccae2
godcandidate <godcandidate@gmail.com>
2025-04-05 13:53:48 +0000

    feat: update meeting route
    - Added a route to get meeting for a ticket

--- routes/meeting.route.ts
+++ routes/meeting.route.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../utils/auth";
-import { createMeeting, getUserMeetings, updateMeetingResolver } from "../controllers/meeting.controller";
+import { createMeeting, getTicketMeeting, getUserMeetings, updateMeetingResolver } from "../controllers/meeting.controller";
 
 const meetingRouter = express.Router();
 
 meetingRouter.post("/meetings", isAuthenticated, createMeeting);
 meetingRouter.get("/meetings/me", isAuthenticated, getUserMeetings);
 meetingRouter.patch("/meetings/:id", isAuthenticated, updateMeetingResolver);
-
+meetingRouter.get("/meetings/ticket/:id", isAuthenticated, getTicketMeeting);
 export default meetingRouter;
