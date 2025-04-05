65acaeb30bfdba197b41b04959f0376626f1c6f6
godcandidate <godcandidate@gmail.com>
2025-04-05 12:43:56 +0000

    feat: update meeting controller
    - Added a route to add a resolver name

--- routes/meeting.route.ts
+++ routes/meeting.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../utils/auth";
-import { createMeeting, getUserMeetings } from "../controllers/meeting.controller";
+import { createMeeting, getUserMeetings, updateMeetingResolver } from "../controllers/meeting.controller";
 
 const meetingRouter = express.Router();
 
 meetingRouter.post("/meetings", isAuthenticated, createMeeting);
 meetingRouter.get("/meetings/me", isAuthenticated, getUserMeetings);
+meetingRouter.patch("/meetings/:id", isAuthenticated, updateMeetingResolver);
 
 export default meetingRouter;
